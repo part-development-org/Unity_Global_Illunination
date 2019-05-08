2837194160de34f9f4659ef3c5469166b51f7bf2
TheProxor <proxortv@gmail.com>
2019-05-08 19:15:19 +0700

    Save shots on HDD from camera

--- ProbeRender.cs
+++ ProbeRender.cs
@@ -11,7 +11,9 @@ namespace TheProxor.GI
     [RequireComponent(typeof(Camera)), ExecuteInEditMode]
     public class ProbeRender : MonoBehaviour
     {
-        internal class CameraState
+        public static ProbeRender instance;
+
+        internal class CameraState : IEnumerable
         {
             private static int counter;
             public Vector3 position { get; private set; }
@@ -40,11 +42,9 @@ namespace TheProxor.GI
                 counter++;
             }
 
-        }
 
-        private readonly CameraState[] cameraStates = new CameraState[]
-        {
-            new CameraState(new Vector3(0, 200, 0), new Vector3(90, 0, 0), 128),
+            internal static readonly IEnumerable<CameraState> cameraStates = new CameraState[]
+            {
             new CameraState(new Vector3(0, 141.5f, 141.5f), new Vector3(135, 0, 0), 128),
             new CameraState(new Vector3(0, 141.5f, -141.5f), new Vector3(45, 0, 0), 128),
             new CameraState(new Vector3(141.5f, 141.5f, 0), new Vector3(45, -90, -90), 128),
@@ -54,22 +54,37 @@ namespace TheProxor.GI
             new CameraState(new Vector3(-100, 141.5f, -100), new Vector3(45, 45, -90), 180),
             new CameraState(new Vector3(100, 141.5f, 100), new Vector3(45, 225, 90), 180),
             new CameraState(Vector3.zero, Quaternion.identity, 128)
-        };
+            };
+
+            internal static IEnumerator enumerator = cameraStates.GetEnumerator();
+            internal static readonly CameraState defaultState = new CameraState(new Vector3(0, 200, 0), new Vector3(90, 0, 0), 128);
+
+            public IEnumerator GetEnumerator()
+            {
+                return enumerator;
+            }
+        }
 
         private Queue<Action<RenderTexture, RenderTexture>> renderActions = new Queue<Action<RenderTexture, RenderTexture>>();
 
         public Camera camera { get { return GetComponent<Camera>(); } }
 
-        public bool NotBakeNow { get { return renderActions.Count == 0; } }
+        public bool NotBakeNow { get { return renderActions == null || renderActions.Count == 0; } }
 
         /// <summary>
         /// Use it for bake GI
         /// </summary>
         public void Bake()
         {
+            renderActions = new Queue<Action<RenderTexture, RenderTexture>>();
             camera.enabled = true;
+            SetCameraState(CameraState.defaultState);
 
-            renderActions.Enqueue(RenderGITextures);
+            CameraState.enumerator.Reset();
+            CameraState.enumerator.MoveNext();
+
+            foreach(var state in CameraState.cameraStates)
+                renderActions.Enqueue(RenderGITexture);
             renderActions.Enqueue(Render3DTextures);
             renderActions.Enqueue(Release);
 
@@ -80,32 +95,32 @@ namespace TheProxor.GI
             }
         }
 
-        private void Update()
-        {
-            
-        }
-
         private void OnRenderImage(RenderTexture source, RenderTexture destination)
         {
-            Debug.Log("Test");
-
             if (!NotBakeNow)          
                 renderActions.Peek().Invoke(source, destination);           
         }
 
         #region Render Actions Methods 
-        private void RenderGITextures(RenderTexture source, RenderTexture destination)
+        private void RenderGITexture(RenderTexture source, RenderTexture destination)
         {
-            Debug.Log("Render GI Textures...");
+            var state = CameraState.enumerator.Current as CameraState;
 
-            foreach(var camState in cameraStates)
-            {
-                camera.orthographicSize = camState.orthoSize;
-                camera.transform.position = camState.position;
-                camera.transform.rotation = camState.rotation;
+            Debug.Log("Render GI Textures for " + state.selfNumber.ToString() + "...");
 
-                Shader.SetGlobalTexture("Texture_" + camState.selfNumber, source);
-            }
+
+            Texture2D tex = new Texture2D(source.width, source.height, TextureFormat.RGB24, false);
+            RenderTexture.active = source;
+            tex.ReadPixels(new Rect(0, 0, source.width, source.height), 0, 0);
+            tex.Apply();
+
+            byte[] bytes = tex.EncodeToPNG();
+
+            System.IO.File.WriteAllBytes(Application.dataPath + "/" +state.selfNumber.ToString() + ".png", bytes);
+
+
+            SetCameraState(state);
+            CameraState.enumerator.MoveNext();
         }
 
         private void Render3DTextures(RenderTexture source, RenderTexture destination)
@@ -121,6 +136,12 @@ namespace TheProxor.GI
 
         #endregion
 
+        private void SetCameraState(CameraState state)
+        {
+            camera.orthographicSize = state.orthoSize;
+            camera.transform.position = state.position;
+            camera.transform.rotation = state.rotation;
+        }
     }
 
 #if UNITY_EDITOR
@@ -153,7 +174,7 @@ namespace TheProxor.GI
         private void SetCameraSettings(Camera camera)
         {
             camera.enabled = false;
-            camera.hideFlags = HideFlags.HideInInspector;
+            camera.hideFlags = HideFlags.None;
             camera.allowHDR = false;
             camera.allowMSAA = false;
             camera.renderingPath = RenderingPath.VertexLit;
@@ -161,6 +182,8 @@ namespace TheProxor.GI
             camera.nearClipPlane = 0;
             camera.farClipPlane = 400;
             camera.orthographicSize = 128;
+            camera.aspect = 1;
+            camera.pixelRect = new Rect(0, 0, 256, 256);
         }
     }
 #endif
